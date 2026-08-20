2bf77ef97c916b0ffd49f8ecfc6c18f6d26de70c
Nguyễn Thị Thu Hiền <nguyentth0909@gmail.com>
2026-08-20 08:08:10 +0700

    task day 1: cấu hình môi trường

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,21 @@
 # Đọc biến môi trường và settings
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    APP_NAME: str = "Event management"
+    APP_VERSION: str = "1.0.0"
+
+    DATABASE_URL: str
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
+    ALLOWED_ORIGINS: list[str] = ["http://localhost:3000", "http://localhost:5173"]
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+settings = Settings()
