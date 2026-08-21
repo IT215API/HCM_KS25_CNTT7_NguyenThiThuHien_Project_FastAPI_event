c411414a9e9c8c19bffdb5f06c65e2fc49b6ac63
Nguyễn Thị Thu Hiền <nguyentth0909@gmail.com>
2026-08-21 23:33:21 +0700

    task day 2: hash password

--- app/core/security.py
+++ app/core/security.py
@@ -1,1 +1,11 @@
 # hash password, JWT encode/decode
+import bcrypt
+import jwt
+from datetime import datetime, timedelta, timezone
+from app.core.config import settings
+
+def hash_password(password: str, cost_factor: int = 12) -> str:
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt(rounds=cost_factor)
+    hashed_bytes = bcrypt.hashpw(password_bytes, salt)
+    return hashed_bytes.decode('utf-8')
