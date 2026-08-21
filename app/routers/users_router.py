917ef0ea7509afad4c1a6c1a6291de6c8fb9384b
Nguyễn Thị Thu Hiền <nguyentth0909@gmail.com>
2026-08-22 00:35:59 +0700

    task day 2: danh sách user, chỉ admin, search theo tên, mail, trạng thái

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,12 +1,13 @@
 # User endpoints
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, Query
 from sqlalchemy.orm import Session
 from app.db.database import get_db
-from app.schemas.user_schema import UserCreate, UserResponse, UserLogin
+from app.schemas.user_schema import UserResponse
 import app.services.user_service as user_service
 from app.schemas.api_schema import success_response
 from app.dependencies.dependencies import RoleChecker, get_current_user
 from app.models.user_model import UserModel
+from typing import Optional
 
 
 router = APIRouter(
@@ -25,5 +26,24 @@ def get_current_user_profile(
     return success_response(
         data=user_data,
         message="Lấy thông tin tài khoản thành công",
+        request=request
+    )
+
+
+@router.get("", response_model=None)
+def get_all_users(
+    request: Request,
+    search: Optional[str] = Query(None, description="Tìm kiếm theo tên hoặc email"),
+    is_active: Optional[bool] = Query(None, description="Lọc theo trạng thái hoạt động (true/false)"),
+    db: Session = Depends(get_db),
+    current_admin: UserModel = Depends(RoleChecker(["Admin"]))
+):
+    users = user_service.get_users(db, search=search, is_active=is_active)
+
+    users_response = [UserResponse.model_validate(u) for u in users]
+
+    return success_response(
+        data=users_response,
+        message="Lấy danh sách người dùng thành công",
         request=request
     )
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -4,6 +4,8 @@ import app.schemas.user_schema as user_schema
 from app.models.user_model import UserModel
 from fastapi import HTTPException, status
 from app.core.security import hash_password
+from typing import Optional
+from sqlalchemy import or_
 
 
 def create_user(db: Session, user: user_schema.UserCreate):
@@ -47,3 +49,24 @@ def authenticate_user(db: Session, user: user_schema.UserLogin) -> dict:
         "access_token": access_token,
         "token_type": "bearer"
     }
+
+
+def get_users(
+    db: Session,
+    search: Optional[str] = None,
+    is_active: Optional[bool] = None
+) -> list[UserModel]:
+    query = db.query(UserModel)
+
+    if search:
+        search_filter = f"%{search.strip()}%"
+        query = query.filter(or_(
+                UserModel.full_name.ilike(search_filter),
+                UserModel.email.ilike(search_filter)
+            )
+        )
+
+    if is_active is not None:
+        query = query.filter(UserModel.is_active == is_active)
+
+    return query.order_by(UserModel.created_at.desc()).all()
